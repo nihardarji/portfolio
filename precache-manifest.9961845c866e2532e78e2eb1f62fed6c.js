feb807dcfdc00baa8a1bfe976c2e4e1290adc0ed
Nihar Darji <nihardarji@Nihars-MacBook-Pro.local>
2020-07-03 17:19:19 -0500

    Updates

---
+++ precache-manifest.9961845c866e2532e78e2eb1f62fed6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c948a3bb859da958720876428b070da",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cefadb4a7ca41d57bba7",
+    "url": "/portfolio/static/css/2.e4363742.chunk.css"
+  },
+  {
+    "revision": "e00d101a2318e39f0faa",
+    "url": "/portfolio/static/css/main.621c4104.chunk.css"
+  },
+  {
+    "revision": "cefadb4a7ca41d57bba7",
+    "url": "/portfolio/static/js/2.94660aa1.chunk.js"
+  },
+  {
+    "revision": "2ecd760a01b9b08172f3fbed44ec5739",
+    "url": "/portfolio/static/js/2.94660aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00d101a2318e39f0faa",
+    "url": "/portfolio/static/js/main.7a240c0f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5478e4282dba81e8badbd9be6fe7b5e5",
+    "url": "/portfolio/static/media/DSC_0144.5478e428.JPG"
+  },
+  {
+    "revision": "c65b99ac4ef07cde99c3538e71f91347",
+    "url": "/portfolio/static/media/Nihar Darji.c65b99ac.pdf"
+  }
+]);
